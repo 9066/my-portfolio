6e382ae1235925801ae990de5360cc4024d06aec
Brajesh Kumar Mishra <brajesh.kumar@inubesolutions.com>
2022-03-08 19:39:31 +0530

    Project Setup

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css"
+function App() {
+  return (
+      <div className="App">
+          <h2> simple test</h2>
+    </div>
+  );
+}
+
+export default App;
